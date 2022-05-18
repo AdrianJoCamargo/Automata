ee493e51791e7482907b3e8004d5d451c38f99b6
AdrianJoCamargo <101289816+AdrianJoCamargo@users.noreply.github.com>
2022-05-18 03:11:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,142 @@
+class Automata():
+    def __init__(self):
+        self.estado=0
+        self.numero=0
+        self.cont=0
+
+    def q0(self,letras):
+        self.estado = 0
+        if (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont+1
+            self.q1(letras)
+        elif(self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q1(self,letras):
+        self.estado = 1
+        if (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q2(letras)
+        elif (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q3(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q2(self,letras):
+        self.estado = 2
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q5(letras)
+        elif (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q4(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q3(self,letras):
+        self.estado = 3
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q6(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q4(self,letras):
+        self.estado = 4
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q3(letras)
+        elif (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q4(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q5(self,letras):
+        self.estado = 5
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q6(letras)
+        elif (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q7(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q6(self,letras):
+        self.estado = 6
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q3(letras)
+        elif (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q4(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q7(self,letras):
+        self.estado = 7
+        if (self.numero != 0 and letras[self.cont] == 'a'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q8(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def q8(self,letras):
+        self.estado = 8
+        if (self.numero != 0 and letras[self.cont] == 'b'):
+            self.numero = self.numero - 1
+            self.cont = self.cont + 1
+            self.q7(letras)
+        elif (self.numero == 0):
+            self.compilador()
+        else:
+            print("Error de escritura")
+
+    def compilador(self):
+        if(self.estado == 3):
+            print("Error, no se llega a un estado valido")
+        else:
+            print("Valor correcto")
+
+    def recorido(self, palabra):
+        lis=list(palabra)
+        self.numero=len(lis)
+        self.cont=0
+        self.q0(lis)
+
+
+auto=Automata()
+auto.recorido("bbbaabaaaabbaa")
+
+
